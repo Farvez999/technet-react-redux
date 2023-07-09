471284d85207b65979e30d12c3855b959613f008
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-07-09 23:21:34 +0600

    RTK query post data

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,14 +1,12 @@
 import ProductReview from '@/components/ProductReview';
 import { Button } from '@/components/ui/button';
-import { useSingleProductsQuery } from '@/redux/api/apiSlice';
-import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
+import { useSingleProductsQuery } from '@/redux/features/product/productApi';
 import { useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
   const { id } = useParams();
 
-  const {data: product,isLoading,error} = useSingleProductsQuery(id)
+  const { data: product, isLoading, error } = useSingleProductsQuery(id);
 
   return (
     <>
@@ -20,14 +18,14 @@ export default function ProductDetails() {
           <h1 className="text-3xl font-semibold">{product?.name}</h1>
           <p className="text-xl">Rating: {product?.rating}</p>
           <ul className="space-y-1 text-lg">
-            {product?.features?.map((feature : string) => (
+            {product?.features?.map((feature: string) => (
               <li key={feature}>{feature}</li>
             ))}
           </ul>
           <Button>Add to cart</Button>
         </div>
       </div>
-      <ProductReview />
+      <ProductReview id={id!} />
     </>
   );
 }
